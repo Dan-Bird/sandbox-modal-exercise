2ba3960090958e03d48072e802be58fc73ae3642
Daniel Bird <info@daniel-bird.co.uk>
2023-04-10 16:34:53 +0100

    Setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+it('should have NewDay as text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByText(/NewDay/)).toBeTruthy();
 });
